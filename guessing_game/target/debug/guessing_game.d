2d1613a37591f5f04113d551fb796091d23428ab
et <eric.thomson13@gmail.com>
2020-07-17 14:25:44 -0600

    target added to .gitignore

--- guessing_game/target/debug/guessing_game.d
+++ guessing_game/target/debug/guessing_game.d
@@ -1,1 +1,1 @@
-/Users/et/Documents/projects/rustExploration/guessing_game/target/debug/guessing_game: /Users/et/Documents/projects/rustExploration/guessing_game/src/main.rs
+/Users/et/Documents/projects/rustExploration/guessing_game/target/debug/guessing_game: /Users/et/Documents/projects/rustExploration/guessing_game/src/main.rs build.rs
