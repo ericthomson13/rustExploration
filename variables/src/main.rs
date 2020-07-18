274123b60651d79864087bd2776875ce97d2438a
et <eric.thomson13@gmail.com>
2020-07-17 18:11:29 -0600

    Basic Variable Traits Added

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,30 @@
 fn main() {
+  // if !mut then will throw compile error
+  // variables are immutable by default
   let mut x = 5;
+
   println!("The valie of x is: {}", x);
-  x = 6;
+  x = x + 1;
   println!("The value of x is: {}", x);
+  x = x * 2;
+  println!("The value of x is: {}", x);
+
+  // const is ALWAYS immutable, not just by default
+  // _ can be used to improve readability
+  const MAX_POINTS: u32 = 100_000;
+
+  println!("The MAX-POINTS value is ALWAYS {}", MAX_POINTS);
+
+  // Can reuse variable name with different type if use let keyword again
+  let spaces = "     ";
+  let spaces = spaces.len();
+
+  println!("There were {} spaces", spaces);
+
+  // Can't make spaces a mutable variable and change the type
+  // Below 2 lines will not compile
+  // let mut spaces = "        ";
+  // spaces = spaces.len();
+
+  println!("There were {} spaces", spaces);
 }
