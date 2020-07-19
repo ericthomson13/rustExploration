ed847f56fe57613be5f1c18a18b11d01a3c2f30d
et <eric.thomson13@gmail.com>
2020-07-19 07:57:40 -0600

    Loops Exploration

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,3 +1,48 @@
 fn main() {
-    println!("Hello, world!");
+  loop_counter();
+  while_coundown(14);
+  for_through_arr();
+}
+
+fn loop_counter () {
+  let mut counter = 0;
+
+  let result = loop {
+    counter += 1;
+
+    if counter == 10 {
+      // without break statement will run infinite loop
+      break counter * 2;
+    }
+  };
+
+  println!("The result is {}", result);
+}
+
+fn while_coundown (num: i32) {
+  println!("Countdown started at {}", num);
+  let mut counter = num;
+
+  while counter != 0 {
+    println!("{}!", counter);
+    counter -= 1;
+  }
+
+  println!("Countdown Complete!")
+}
+
+fn for_through_arr () {
+  let arr = [10, 20, 30, 40, 50];
+
+  for elem in arr.iter() {
+    println!("Value from arr is {}", elem);
+  }
+}
+
+fn for_liftoff () {
+  // (1..4) uses a range and then .rev() goes from upper to lower both are std lib
+  for number in (1..4).rev() {
+    println!("{}!", number);
+  }
+  println!("Liftoff!")
 }
